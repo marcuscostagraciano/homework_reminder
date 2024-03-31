ce9f62fd8d6433365377d913dfce2a4b495b80c3
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-30 23:51:59 -0300

    basic configs

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,8 +1,30 @@
 <script setup>
+import { routes } from '../router'
 
 </script>
 
 <template>
+    <v-app-bar density="comfortable">
+        <v-col>
+            <v-app-bar-title class="font-weight-bold">
+                Homework Reminder
+            </v-app-bar-title>
+        </v-col>
+
+        <v-col>
+            <v-btn v-for="route in routes" class="px-2 py-0">
+                <router-link :to="route.path">{{ route.name }}</router-link>
+            </v-btn>
+        </v-col>
+    </v-app-bar>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-col {
+    text-align: center
+}
+
+a {
+    text-decoration: none;
+}
+</style>
